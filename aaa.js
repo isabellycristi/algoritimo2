99b29e49ef1670879781136e8ab6f55a8deb461f
isabellycristi <isabellysanxes@gmail.com>
2024-09-24 12:52:45 -0400

    Add files via upload

---
+++ aaa.js
@@ -0,0 +1,6 @@
+var alunos=["Maria", "João", "Ana", "Pedro"];
+console.log(alunos);
+console.log(alunos[2]);
+console.log(alunos.length);
+alunos[1]=('Adriana')
+console.table(alunos);
---
+++ bbb.js
@@ -0,0 +1,6 @@
+var boletim = [[8,7,9,3],[4,5,8,6]];
+console.log(boletim);
+console.log(boletim[1][2]);
+console.table(boletim);
+boletim[1][2]=10;
+console.table(boletim);
